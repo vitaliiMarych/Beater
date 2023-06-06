973ba960206f10700c733bbda446190954c9c5dd
Vitalii Marych <apiskin557@gmail.com>
2023-06-06 19:28:28 +0300

    bootstrap4 -> bootstrap5

---
+++ Training/src/main/java/com/example/biter/Service/MessageService.java
@@ -0,0 +1,64 @@
+package com.example.biter.Service;
+
+import com.example.biter.Domain.Message;
+import com.example.biter.Domain.User;
+import com.example.biter.Repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+public class MessageService {
+    @Autowired
+    private MessageRepo messageRepo;
+
+    @Value("${upload.path}")
+    private String uploadPath;
+
+    public void saveFile(Message message, MultipartFile file) throws IOException {
+        if (file != null && !file.getOriginalFilename().isEmpty()) {
+            File uploadDir = new File(uploadPath);
+
+            if (!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+
+            String uuidFile = UUID.randomUUID().toString();
+            String fileName = uuidFile + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath + "/" + fileName));
+
+            message.setFilename(fileName);
+        }
+    }
+
+    public void addMessage(Message message, User user, MultipartFile file) throws IOException {
+
+        message.setAuthor(user);
+
+        saveFile(message, file);
+
+        messageRepo.save(message);
+    }
+
+    public Iterable<Message> findAll() {
+        return messageRepo.findAll();
+    }
+
+    public Iterable<Message> findByTagLike(String tag) {
+        return messageRepo.findByTagLike(tag);
+    }
+
+    public Message findById(Long id){
+        return messageRepo.findById(id).orElse(null);
+    }
+
+    public void save(Message message) {
+        messageRepo.save(message);
+    }
+}
--- Training/target/classes/db/migration/V1__Init_DB.sql
+++ Training/target/classes/db/migration/V1__Init_DB.sql
@@ -26,6 +26,12 @@ create table usr (
     primary key (id)
 );
 
+create table user_subscriptions
+(
+    channel_id bigint not null references usr,
+    subscriber_id bigint not null references usr,
+    primary key (channel_id, subscriber_id)
+);
 
 alter table message
     add constraint message_usr_fk
